e9f567f573a3afa8b761566dad95a01a8c2f5bcb
DoanMinhCuong <minhcuong3859@gmail.com>
2023-07-20 17:25:38 +0700

    loginupdate

--- newcloudnote/src/pages/index.js
+++ newcloudnote/src/pages/index.js
@@ -1,9 +1,29 @@
+import React, { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
 export default function Index() {
-    return(
+    // const [imageSrc, setImageSrc] = useState('/images/Rectangle 1.svg');
+    const [isHovered, setIsHovered] = useState(false);
+    const imageProps = useSpring({
+        transform: isHovered ? 'scale(1.2)' : 'scale(1)',
+      });
+    
+      const handleMouseOver = () => {
+        setIsHovered(true);
+      };
+    
+      const handleMouseOut = () => {
+        setIsHovered(false);
+      };
+    return (
+
         <>
-        <div className="content-cloud">
-            
-        </div>
+            <div className="content-cloud">
+                <img src={imageSrc}
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                    alt="Image" />
+
+            </div>
         </>
     )
 }
