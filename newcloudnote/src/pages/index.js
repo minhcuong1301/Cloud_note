2dff746333274fa6ba5d96f4c6f4c33831cd7e7d
DoanMinhCuong <minhcuong3859@gmail.com>
2023-07-20 17:57:03 +0700

    imghome

--- newcloudnote/src/pages/index.js
+++ newcloudnote/src/pages/index.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 export default function Index() {
-    return(
+    const [imageSrc, setImageSrc] = useState('/images/Rectangle 1.svg');
+
+    const handleMouseOver = () => {
+        setImageSrc('/images/Rectangle 2.svg');
+    };
+
+    const handleMouseOut = () => {
+        setImageSrc('/images/Rectangle 1.svg');
+    };
+    return (
+
         <>
-        <div className="content-cloud">
-            
-        </div>
+            <div className="content-cloud">
+                <img
+                    src={imageSrc}
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}
+                    alt="Image"
+                />
+            </div>
         </>
     )
 }
