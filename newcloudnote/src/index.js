4424f32d2bcab8c16d4b9b5ec40a53ae53921ab0
Tuan <dinhkhactuan2002@gmail.com>
2023-07-20 16:13:11 +0700

    Update code page Login

--- newcloudnote/src/index.js
+++ newcloudnote/src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import router from './router/router';
-import "./assets/scss/index.scss"
-import { RouterProvider } from 'react-router-dom';
-ReactDOM.createRoot(document.getElementById('root')).render(
-        <RouterProvider router={router}/>
-  
+import React from "react";
+import ReactDOM from "react-dom/client";
+import router from "./router/router";
+import "./assets/scss/index.scss";
+import { RouterProvider } from "react-router-dom";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
-
-
--- newcloudnote/src/router/router.js
+++ newcloudnote/src/router/router.js
@@ -1,17 +1,22 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Index from "../pages";
 import Layout from "../pages/layout";
+import Page_login from "../pages/page_Login/Login";
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children:[
-            {
-                index: true,
-                element: <Index/>
-            },
-        ]
+  {
+    path: "/",
+    element: <Layout />,
 
-    }
-])
-export default router
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Page_login />,
+  },
+]);
+export default router;
